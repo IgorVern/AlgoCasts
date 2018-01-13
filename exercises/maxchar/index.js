d71f1bbd6f8f3cf5fe4265be37b28e807817d969
Igor Vernichenko <igor_vernichenko@epam.com>
2018-01-14 01:19:18 +0300

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+	const countedChars =  str.split('').reduce((acc, current) => {
+		if (!acc[current]) {
+			acc[current] = 1;
+		} else {
+			acc[current]++;
+		}
+		return acc;
+	}, {});
+
+	let max = 0, key;
+
+	Object.keys(countedChars).forEach(k => {
+		if (countedChars[k] > max) {
+			max = countedChars[k];
+			key = k;
+		}
+	});
+
+	return key;
+}
 
 module.exports = maxChar;
