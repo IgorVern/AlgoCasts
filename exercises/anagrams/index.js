4850627052aea25dc5c4d233a92aec9bf7a0b716
Igor Vernichenko <igor_vernichenko@epam.com>
2018-01-14 14:27:41 +0300

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,33 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+const transformString = (str) => str.replace(/[^a-z]*/gi).toLocaleLowerCase().split('').sort().join('');
+
+function anagrams(stringA, stringB) {
+	return transformString(stringA) === transformString(stringB);
+}
+//
+// function anagrams(stringA, stringB) {
+// 	const countedA = countChars(stringA.replace(/[^a-z]*/gi).toLocaleLowerCase());
+// 	const countedB = countChars(stringB.replace(/[^a-z]*/gi).toLocaleLowerCase());
+// 	const keysA = Object.keys(countedA);
+// 	const keysB = Object.keys(countedB);
+// 	if (keysA.length !== keysB.length) {
+// 		return false;
+// 	}
+//
+// 	return keysA.every(key => countedA[key] === countedB[key]);
+// }
+//
+// function countChars(str) {
+// 	return str.split('').reduce((acc, curr) => {
+// 		if (!acc[curr]) {
+// 			acc[curr] = 1;
+// 		} else {
+// 			acc[curr]++;
+// 		}
+// 		return acc;
+// 	}, {});
+// }
 
 module.exports = anagrams;
