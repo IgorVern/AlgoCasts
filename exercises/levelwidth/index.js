b2c0832de4221a599d58b3649c1501ae3bf414ec
Igor Vernichenko <igor.vernichenko@novartis.com>
2018-02-27 15:24:10 +0300

    levelwidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,24 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const STOP = 'stop'
+  const counter = [0]
+  const nodes = [root, STOP]
+
+  while (nodes.length > 1) {
+    const node = nodes.shift()
+
+    if (node === STOP) {
+      nodes.push(node)
+      counter.push(0)
+    } else {
+      nodes.push(...node.children)
+      counter[counter.length - 1]++
+    }
+  }
+
+  return counter
+}
 
 module.exports = levelWidth;
